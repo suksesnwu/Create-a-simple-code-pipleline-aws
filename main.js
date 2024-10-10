a8baeb6c60f84c62071273f644ee64f1e4dfab35
Katleho <katlehomokhethi1@gmail.com>
2024-10-10 10:34:27 +0200

    Add simple web app files

---
+++ main.js
@@ -0,0 +1,4 @@
+// main.js
+document.getElementById('actionButton').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'Button clicked! Welcome to my simple web app!';
+});
